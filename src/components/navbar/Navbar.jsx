1c0e9aa93659e4e924b3eb0ba2ccc87f74aca421
sava <savamedvedev@gmail.com>
2022-04-26 23:30:03 +0800

    second-commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { BiSearch } from "react-icons/bi";
+import { BsPerson } from "react-icons/bs";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaPinterest,
+  FaYoutube,
+} from "react-icons/fa";
 import "./NavbarStyles.css";
 
 function Navbar() {
@@ -15,8 +25,35 @@ function Navbar() {
         <li>Views</li>
       </ul>
       <div className="nav-icons">
-		  
-	  </div>
+        <BiSearch className="icon" />
+        <BsPerson className="icon" />
+      </div>
+      <div className="hamburger">
+        <HiOutlineMenuAlt4 className="icon" />
+      </div>
+
+      <div className="mobile-menu">
+        <ul className="mobile-nav">
+          <li>Home</li>
+          <li>Destination</li>
+          <li>Travel</li>
+          <li>Book</li>
+          <li>Views</li>
+        </ul>
+        <div className="mobile-menu-bottom">
+          <div className="menu-icons">
+            <button>Search</button>
+            <button>Account</button>
+          </div>
+          <div className="social-icons">
+            <FaFacebook className="icon" />
+            <FaInstagram className="icon" />
+            <FaTwitter className="icon" />
+            <FaPinterest className="icon" />
+            <FaYoutube className="icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
