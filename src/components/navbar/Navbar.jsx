3bbded2562cb76b75f99d43158164f047e1fd579
sava <savamedvedev@gmail.com>
2022-04-26 21:58:39 +0800

    first-commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./NavbarStyles.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h2>BETCHES.</h2>
+      </div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Destination</li>
+        <li>Travel</li>
+        <li>Book</li>
+        <li>Views</li>
+      </ul>
+      <div className="nav-icons">
+		  
+	  </div>
+    </div>
+  );
+}
+
+export default Navbar;
