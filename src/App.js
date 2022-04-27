ecf585844db160b0ade43c4d65cd40c7ebdb5478
sava <savamedvedev@gmail.com>
2022-04-27 20:56:48 +0800

    fives-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Search from "./components/search/Search";
+import Destinations from "./components/destinations/Destinations";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Destinations />
+      <Search />
     </>
   );
 }
